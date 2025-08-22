8893e829dc17caa9b9a061be88c0055823d4c74b
Florian <florian.duruz@gmail.com>
2025-08-22 14:45:22 +0200

    Merge Monstres

--- BloomAndDoomGame/Assets/ProceduralGeneration/RoomGeneration.cs
+++ BloomAndDoomGame/Assets/ProceduralGeneration/RoomGeneration.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 public class RoomGeneration : MonoBehaviour
 {
+    private NavMeshSurface navMesh;
+    
     [Min(1)] public int Level = 1;
     
     public RoomCollection RoomCollection;
 
     public List<RoomSpawner> Rooms = new List<RoomSpawner>(5);
     public List<RoomSpawner> BossRooms = new List<RoomSpawner>();
+
+    private void Awake()
+    {
+        navMesh = GetComponent<NavMeshSurface>();
+    }
 
     private void Start()
     {
@@ -32,6 +40,15 @@ public class RoomGeneration : MonoBehaviour
             yield return new WaitForSeconds(2f);
         }
         BossRooms.Add(Rooms[^1]);
+        navMesh.BuildNavMesh();
+        Vector3[] positions = new[]
+        {
+            Vector3.back * 10,
+            Vector3.forward * 10,
+            Vector3.left * 10,
+            Vector3.right * 10, 
+        };
+        GameManager.Instance.InstantiateMonsters(positions);
     }
 
     private void GenerateBossRoom()
